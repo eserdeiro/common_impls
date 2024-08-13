7c9bad3bd03dfdef93221da0f13f4f38f9fc7e2f
eserdeiro <eserdeiro@gmail.com>
2024-08-13 17:34:05 -0300

    .

--- lib/common_impls.dart
+++ lib/common_impls.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
 class CommonImpls {
+  CommonImpls._privateConstructor(); // Private constructor
+
+  static final CommonImpls _instance = CommonImpls._privateConstructor();
+
+  factory CommonImpls() {
+    return _instance;
+  }
   final GlobalKey<NavigatorState> globalNavigatorKey =
       GlobalKey<NavigatorState>();
 }
